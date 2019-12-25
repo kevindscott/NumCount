3326c047835b6bc737472dc69442833951143af9
kevindscott <kevinscott.collcetdat@gmail.com>
2019-12-24 22:24:28 -0500

    Complete fun test

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace NumCount
 {
@@ -6,7 +7,36 @@ namespace NumCount
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string numberInput = "";
+            if(args.Length > 0){
+                numberInput = args[0];
+                string comdinedNum = "";
+
+                Console.WriteLine(numberInput);
+
+                for(int i=0; i < 3; i++){
+
+                    if(comdinedNum != ""){
+                        numberInput = comdinedNum;
+                    }
+
+                    List<string> chars = new List<string>();
+                    comdinedNum = "";
+                    foreach(char ch in numberInput.ToCharArray()){
+                        int charCount = 0;
+
+                        charCount = numberInput.Length - numberInput.Replace(ch.ToString(),"").Length;
+                        if(!chars.Contains(ch + charCount.ToString())){
+                            chars.Add(ch + charCount.ToString());
+                            comdinedNum += ch + charCount.ToString();
+                        }
+
+                    }
+
+                    Console.WriteLine(comdinedNum);
+                }
+            }
+
         }
     }
 }
